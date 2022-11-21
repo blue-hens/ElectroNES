9c0c4d4b1713f7cf0198dadfab61ee41141409aa
Andy Novocin <andy@novocin.com>
2022-11-21 10:14:25 -0500

    launch scripts

---
+++ makearttest.sh
@@ -0,0 +1,4 @@
+ca65 famistudio_ca65.s
+ca65 arttesting/reset.asm
+ca65 arttesting/proj.asm
+ld65 arttesting/reset.o arttesting/proj.o famistudio_ca65.o -C nes.cfg -o workingart.nes
---
+++ spritetest.sh
@@ -0,0 +1,4 @@
+ca65 famistudio_ca65.s
+ca65 spritetesting/reset.asm
+ca65 spritetesting/proj.asm
+ld65 spritetesting/reset.o spritetesting/proj.o famistudio_ca65.o -C nes.cfg -o workingsprite.nes
